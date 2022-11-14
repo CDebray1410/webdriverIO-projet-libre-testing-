faa76b5fe8065d2187f43e4028f99753cf85f776
Christopher Debray <christopher.debray@club-employes.com>
2022-11-14 16:13:22 +0100

    CHANGE now will use steps/file separation

---
+++ features/pageobjects/admin/materCreationTest.ts
@@ -0,0 +1,27 @@
+import { Given, When, Then } from '@wdio/cucumber-framework';
+
+import BasePage from '../../pageobjects/base/base.page';
+import MaterCreationPage from '../../pageobjects/admin/materCreation.page';
+
+const pages = {
+    base: BasePage,
+    matterCreation: MaterCreationPage
+}
+
+const urlBase = "http://localhost:1373/";
+
+Given(/^I am on the (\w+) page$/, async (page) => {
+    await pages[page].open()
+});
+
+When(/^I create a matter as admin$/, async () => {
+    await BasePage.login('admin');
+    await browser.pause(500);
+    await MaterCreationPage.createMatter();
+});
+
+Then(/^I should see a success alert for mater$/, async (status) => {
+    await browser.pause(1000);
+    const successAlertBox = await $('#success-alert-box')
+    expect(successAlertBox).toHaveElementClassContaining('displayed')
+});
